ddc221d1b669714b274d221167316202db919336
Blake Caldwell <blakecaldwell123@gmail.com>
2020-11-17 19:26:50 -0800

    adds requirements for task1
    Added the new commands remove, add, display, reverse, and count

--- Threads/src/main/java/Performer.java
+++ Threads/src/main/java/Performer.java
@@ -1,4 +1,6 @@
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.*;
 
 class Performer {
@@ -28,9 +30,69 @@ class Performer {
 
                 if (str == null || str.equals("."))
                     done = true;
+                else if(str.contains("add")) {
+                	try {
+	                	// get the string without space or add
+	                	String input = str.split("add")[1].split(" ")[1];
+	                	state.add(input);
+	                	out.println("Server state is now: " + state.toString());
+                	}catch(ArrayIndexOutOfBoundsException e) {
+                		out.println("No word was given to add!");
+                	}
+                }
+                else if(str.contains("remove")) {
+                	try {
+                		String input = str.split("remove")[1].split(" ")[1];
+	                	int index = Integer.parseInt(input);
+	                	if(index < state.size()) {
+	                		String word = state.getIndex(index);
+	                		state.remove(index);
+	                		out.println("Removed " + word);
+	                	}else {
+	                		out.println("No such index");
+	                	}
+                	}catch(NumberFormatException e) {
+                		out.println("No number sent please send a number for an index");
+                	}catch(ArrayIndexOutOfBoundsException e) {
+                		out.println("No number sent please send a number for an index");
+                	}
+                }
+                else if(str.contains("display")) {
+                	out.println("Server state is now: " + state.toString());	
+                }
+                else if(str.contains("reverse")) {
+                	try {
+	                	String input = str.split("reverse")[1].split(" ")[1];
+	                	int index = Integer.parseInt(input);
+	                	
+	                	if(index < state.size()) {
+	                	
+	                		String word = state.getIndex(index);
+	                		String newWord = "";
+	                		for(int i=word.length()-1;i>=0;i--) {
+	                			newWord = newWord + word.charAt(i);
+	                		}
+	                		
+	                		state.set(index, newWord);
+	                		out.println("Server state is now: " + state.toString());	
+	                	}else {
+	                		out.println("No such index");
+	                	}
+                	}catch(NumberFormatException e) {
+                		out.println("No number sent please send a number for an index");
+                	}catch(ArrayIndexOutOfBoundsException e) {
+                		out.println("No number sent please send a number for an index");
+                	}
+                }
+                else if(str.contains("count")) {
+                	List<Integer> response = new ArrayList<Integer>();
+                	for(int i=0;i<state.size();i++) {
+                		response.add(state.getIndex(i).length());
+                	}
+                	out.println("Server state count is: " + response.toString());
+                }
                 else {
-                    state.add(str);
-                    out.println("Server state is now: " + state.toString());
+                    out.println("Unknown Command try: add <String>, remove <int>, display, count, reverse <int>");
                 }
             }
         } catch (IOException e) {
--- Threads/src/main/java/StringList.java
+++ Threads/src/main/java/StringList.java
@@ -16,6 +16,29 @@ class StringList {
     public boolean contains(String str) {
         return strings.indexOf(str) >= 0;
     }
+    
+    public String getIndex(int i) {
+    	if(i<size()) {
+    		return strings.get(i);	
+    	}
+    	return null;
+    }
+    
+    public boolean remove(int i) {
+    	if(i<size()) {
+    		strings.remove(i);
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public boolean set(int i, String in) {
+    	if(i<size() && in.length() > 0) {
+    		strings.set(i, in);
+    		return true;
+    	}
+    	return false;
+    }
 
     public int size() {
         return strings.size();
